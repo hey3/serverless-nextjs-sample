93ecfa467558a940c4f97e826dedd41d249c39a8
Kohei Oyama <kohei.oyama@access-company.com>
2020-12-07 20:59:28 +0900

    remove sample configs (#5)

--- serverless.js
+++ serverless.js
@@ -6,7 +6,7 @@ class Deploy extends Component {
   async default(inputs = {}) {
     const { stage } = inputs
     if (validEnvs.includes(stage)) {
-      require('dotenv').config({ path: `${__dirname}/.env-${stage}` })
+      require('dotenv').config({ path: `${__dirname}/env-${stage}` })
 
       const template = await this.load('@serverless/template', stage)
       return await template({ template: 'serverless.yml' })
